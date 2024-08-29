9d130b809bec9373232cdeb6ffe4118cbdaa19a7
Leonardo Zamarchi <leonardozamarchi@gmail.com>
2024-08-29 13:03:32 -0300

    ajusts

--- virus_propagation.py
+++ virus_propagation.py
@@ -19,26 +19,29 @@ def run_simulation(taxa_infeccao):
     infected_indices = np.random.choice(grid_size[0] * grid_size[1], 100, replace=False)
     population[infected_indices] = 1
 
+    # Lista para armazenar a entropia em cada iteração
+    entropias = []
+
     # Configurar a figura para a animação, slider e botões
     fig, ax = plt.subplots()
     plt.subplots_adjust(left=0.25, bottom=0.5)  # Ajustar para espaço dos controles
     im = ax.imshow(population.reshape(grid_size), cmap='viridis', vmin=0, vmax=3)
-    ax.set_title('Simulação da Propagação de Vírus com Fatalidade', fontsize=16)
+    ax.set_title('Simulação da Propagação de Vírus', fontsize=16)
     
     # Descrição das cores na legenda
     colorbar = fig.colorbar(im, ax=ax)
     colorbar.set_ticks([0, 1, 2, 3])
-    colorbar.set_ticklabels(['Suscetível', 'Infectado', 'Recuperado', 'Falecido'])
+    colorbar.set_ticklabels(['Suscetível', 'Infectado', 'Recuperado', 'Óbito'])
 
     # Adicionar sliders e botões
     ax_slider_infeccao = plt.axes([0.25, 0.35, 0.65, 0.03], facecolor='lightgoldenrodyellow')
     taxa_infeccao_slider = Slider(ax_slider_infeccao, 'Taxa de Infecção', 0, 0.1, valinit=taxa_infeccao)
     
     ax_slider_recuperacao = plt.axes([0.25, 0.25, 0.65, 0.03], facecolor='lightblue')
-    taxa_recuperacao_slider = Slider(ax_slider_recuperacao, 'Taxa de Recuperação', 0, 0.5, valinit=0.1)
+    taxa_recuperacao_slider = Slider(ax_slider_recuperacao, 'Taxa de Recuperação', 0, 0.5, valinit=0.2)
     
     ax_slider_fatalidade = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor='lightcoral')
-    taxa_fatalidade_slider = Slider(ax_slider_fatalidade, 'Taxa de Fatalidade', 0, 0.1, valinit=0.05)
+    taxa_fatalidade_slider = Slider(ax_slider_fatalidade, 'Taxa de Fatalidade', 0, 0.1, valinit=0.03)
 
     # Botões de Play/Pause e Reset
     ax_play = plt.axes([0.25, 0.05, 0.1, 0.04])
@@ -68,6 +71,10 @@ def run_simulation(taxa_infeccao):
             fatalidade_indices = np.random.choice(infected_indices, int(len(infected_indices) * taxa_fatalidade_slider.val), replace=False)
             population[fatalidade_indices] = 3
             
+            # Calcular e armazenar a entropia
+            entropia = calcular_entropia_virus(population)
+            entropias.append(entropia)
+
             # Atualizar a imagem
             im.set_data(population.reshape(grid_size))
             
@@ -82,10 +89,11 @@ def run_simulation(taxa_infeccao):
         is_paused = True
 
     def reset(event):
-        nonlocal population
+        nonlocal population, entropias
         population = np.zeros(grid_size[0] * grid_size[1])
         infected_indices = np.random.choice(grid_size[0] * grid_size[1], 100, replace=False)
         population[infected_indices] = 1
+        entropias.clear()  # Limpar a lista de entropias ao resetar
         im.set_data(population.reshape(grid_size))
         plt.draw()
 
@@ -98,5 +106,13 @@ def run_simulation(taxa_infeccao):
 
     plt.show()
 
+    # Após fechar a animação, exibir o gráfico de entropia
+    plt.figure()
+    plt.plot(entropias)
+    plt.xlabel('Iteração')
+    plt.ylabel('Entropia')
+    plt.title('Entropia na Propagação de um Vírus')
+    plt.show()
+
 # Rodar a simulação com o valor inicial do slider
-run_simulation(0.01)
+run_simulation(0.02)
